5660a8b292e82b6a7aa971cb196d831100925a0b
chenweibo <563960993@qq.com>
2017-11-21 10:50:49 +0800

    add mail

--- config/backup.php
+++ config/backup.php
@@ -92,7 +92,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => '563960993@qq.com',
         ],
 
         'slack' => [
